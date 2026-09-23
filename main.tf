4dd8bf42ef05ad7dc34a9c0398149e0cedf78872
joylearn <joylearn@hotmail.comyour-github-username@users.noreply.github.com>
2026-09-24 01:26:25 +0800

    include required provider & version

--- main.tf
+++ main.tf
@@ -1,4 +1,11 @@
 terraform {
+  required_version = ">= 1.5.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
   backend "s3" {
     bucket = "sctp-tfstate-ce13"
     key    = "jl/32-workflow.tfstate"

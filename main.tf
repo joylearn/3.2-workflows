3a9bc4674703182a204556677708914d950b0717
joylearn <joylearn@hotmail.comyour-github-username@users.noreply.github.com>
2026-09-23 23:51:57 +0800

    Adding new s3 bucket

---
+++ main.tf
@@ -0,0 +1,16 @@
+terraform {
+  backend "s3" {
+    bucket       = "sctp-tfstate-ce13"
+    key          = "jl/32-workflow.tfstate"
+    region       = "us-east-1"
+  }
+}
+
+provider "aws" {
+   region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "workshop" {
+  bucket_prefix = "jl-32-workflows"
+}
+
